c0302af3bc17150cdd0da5de5f3795670bebd9c4
nguyenthethinh <nguyenthethinh@gmail.com>
2016-02-25 15:07:51 -0800

    Exercise 2 completed

---
+++ 02_Exercise/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	intV := 2016
+	floatV := 2016.0
+	stringV := "This is a string"
+	boolV := true
+	arrayV := []int{1,2,3}
+	fmt.Printf("%T \t %T \t %T \t %T \t %T \n", intV, floatV, stringV, boolV, arrayV )
+
+}
