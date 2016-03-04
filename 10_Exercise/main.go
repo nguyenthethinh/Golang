4a506365d8c902f9dad23bccb98357ae2a9f9a6c
nguyenthethinh <nguyenthethinh@gmail.com>
2016-03-04 15:00:33 -0800

    Display the name in the url when the url is localhost:8080?n=name

---
+++ 10_Exercise/main.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+	"net/http"
+	"fmt"
+	"io"
+)
+
+func main(){
+	http.HandleFunc("/", func(res http.ResponseWriter, req * http.Request) {
+		fmt.Println(req.URL.Path)
+		fmt.Println(req.URL.RequestURI())
+		io.WriteString(res, "Name: " + req.FormValue("n"))
+	})
+	http.ListenAndServe(":8080", nil)
+}
